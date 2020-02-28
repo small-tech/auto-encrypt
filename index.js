f1babd7cc4fdda191d3bcb70c371eedd4b3069cf
Aral Balkan <mail@ar.al>
2020-02-28 13:30:36 +0000

    Progress on creating signed request

--- index.js
+++ index.js
@@ -4,6 +4,9 @@
 const util = require('util')
 const prepareRequest = require('bent')
 
+const jose = require('jose')
+const base64url = require('base64url')
+
 const Identity = require('./lib/Identity')
 const Directory = require('./lib/Directory')
 
@@ -27,7 +30,42 @@ async function main () {
 
   console.log('New nonce', newNonce)
 
+  const payload = { termsOfServiceAgreed: true }
+  const protectedHeader = {
+    alg: 'EdDSA',
+    crv: 'Ed25519',
+    jwk: identity.publicKey,
+    nonce: newNonce,
+    url: directory.newAccountUrl
+  }
 
+  console.log('payload', payload)
+  console.log('protectedHeader', protectedHeader)
+
+  const signedRequest = jose.JWS.sign.flattened(payload, identity.OKPKey, protectedHeader)
+  signedRequest.protected = base64url(signedRequest.protected)
+  signedRequest.payload = base64url(signedRequest.payload)
+
+  console.log('Signed request', signedRequest)
+
+  // Prepare a new account request (RFC § 7.3)
+  // const newAccountRequest = prepareRequest('POST', directory.newAccountUrl, 'json', /* acceptable responses are */ 200, 201, {
+  //   protected: base64url({
+  //     alg: 'EdDSA',
+  //     crv: 'Ed25519',
+  //     jwk: identity.publicKey,
+  //     nonce: newNonce,
+  //     url: directory.newAccountUrl
+  //   }),
+  //   payload: base64url({
+  //     termsOfServiceAgreed: true
+  //   }),
+  //   signature: ''
+  // })
+
+  // const newAccountResponse = await newAccountRequest()
+
+  // console.log('New account response', newAccountResponse)
 }
 
 main()
