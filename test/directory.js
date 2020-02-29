065d85c757dc0e31ec6eda340dc7fee9d2327fcb
Aral Balkan <mail@ar.al>
2020-02-29 19:11:54 +0000

    Add remaining tests for Directory class

--- test/directory.js
+++ test/directory.js
@@ -2,8 +2,8 @@ const test = require('tape')
 
 const Directory = require('../lib/Directory')
 
-test('directory', async t => {
-  t.plan = 13
+test('directory' /* , {skip: true} */, async t => {
+  t.plan = 23
 
   t.strictEquals(Directory.instance, null, 'directory singleton instance is null to begin with')
 
@@ -26,13 +26,13 @@ test('directory', async t => {
   const stagingWebsiteUrl = 'https://letsencrypt.org/docs/staging-environment/'
   const termsOfServiceUrl = 'https://letsencrypt.org/documents/LE-SA-v1.2-November-15-2017.pdf'
 
-  const url = p => `${stagingBaseUrl}/${p}`
+  const stagingUrl = p => `${stagingBaseUrl}/${p}`
 
-  t.strictEquals(stagingDirectory.keyChangeUrl, url('key-change'), 'staging key change url is as expected')
-  t.strictEquals(stagingDirectory.newAccountUrl, url('new-acct'), 'staging new account url is as expected')
-  t.strictEquals(stagingDirectory.newNonceUrl, url('new-nonce'), 'staging new nonce url is as expected')
-  t.strictEquals(stagingDirectory.newOrderUrl, url('new-order'), 'staging new order url is as expected')
-  t.strictEquals(stagingDirectory.revokeCertUrl, url('revoke-cert'), 'staging revoke cert url is as expected')
+  t.strictEquals(stagingDirectory.keyChangeUrl, stagingUrl('key-change'), 'staging key change url is as expected')
+  t.strictEquals(stagingDirectory.newAccountUrl, stagingUrl('new-acct'), 'staging new account url is as expected')
+  t.strictEquals(stagingDirectory.newNonceUrl, stagingUrl('new-nonce'), 'staging new nonce url is as expected')
+  t.strictEquals(stagingDirectory.newOrderUrl, stagingUrl('new-order'), 'staging new order url is as expected')
+  t.strictEquals(stagingDirectory.revokeCertUrl, stagingUrl('revoke-cert'), 'staging revoke cert url is as expected')
   t.strictEquals(stagingDirectory.websiteUrl, stagingWebsiteUrl, 'staging web site url is as expected')
   t.strictEquals(stagingDirectory.termsOfServiceUrl, termsOfServiceUrl, 'terms of service url is as expected')
 
@@ -50,6 +50,24 @@ test('directory', async t => {
   Directory.isReady = false
 
   const productionDirectory = await Directory.getSharedInstance(/* staging = */ false)
+
+  t.strictEquals(productionDirectory.isProduction, true, 'directory singleton instance manually set to LE production server')
+  t.strictEquals(productionDirectory.isStaging, false, 'directory singleton instance is not LE staging server')
+  t.strictEquals(productionDirectory.acmeEndpoint, Directory.PRODUCTION_ENDPOINT, 'the production endpoint is correctly set')
+
+  // Also test the actual Urls returned just so we’re notified if anything chances on their end
+  // (the returned URLs are subject to change but it is unlikely).
+  const productionBaseUrl = 'https://acme-v02.api.letsencrypt.org/acme'
+  const productionWebsiteUrl = 'https://letsencrypt.org'
+
+  const productionUrl = p => `${productionBaseUrl}/${p}`
+
+  t.strictEquals(productionDirectory.keyChangeUrl, productionUrl('key-change'), 'production key change url is as expected')
+  t.strictEquals(productionDirectory.newAccountUrl, productionUrl('new-acct'), 'production new account url is as expected')
+  t.strictEquals(productionDirectory.newNonceUrl, productionUrl('new-nonce'), 'production new nonce url is as expected')
+  t.strictEquals(productionDirectory.newOrderUrl, productionUrl('new-order'), 'production new order url is as expected')
+  t.strictEquals(productionDirectory.revokeCertUrl, productionUrl('revoke-cert'), 'production revoke cert url is as expected')
+  t.strictEquals(productionDirectory.websiteUrl, productionWebsiteUrl, 'production web site url is as expected')
 
   t.end()
 })
