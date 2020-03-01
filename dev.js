830b3fc494dca6eb689122545d606c50ebdfb33a
Aral Balkan <mail@ar.al>
2020-03-01 18:42:45 +0000

    Refactor to create class for package; add Account class; etc.
    Functioning correctly. Tests failing.

---
+++ dev.js
@@ -0,0 +1,18 @@
+//////////  //////////  //////////  //////////  //////////  //////////
+//
+// !!! Bootstrap for testing during initial development. !!!
+//
+// TODO: Remove once library complete.
+//
+//////////  //////////  //////////  //////////  //////////  //////////
+
+const AcmeHttp01 = require('./index')
+
+async function main () {
+  // newAccount
+  const acmeHttp01 = await AcmeHttp01.getSharedInstance()
+}
+
+main()
+
+//////////  //////////  //////////  //////////  //////////  //////////
--- index.js
+++ index.js
@@ -16,164 +16,63 @@
 //
 ////////////////////////////////////////////////////////////////////////////////
 
-const util = require('util')
-const prepareRequest = require('bent')
+const os = require('os')
+const path = require('path')
+const fs = require('fs-extra')
 
-const jose = require('jose')
+// Continue to end of file to see the rest of the dependencies. The ones at the
+// end are there as they require a reference to this class.
+// (This is a limitation of Node requires. See https://stackoverflow.com/a/21334734)
 
-const log = require('./lib/log')
-const Identity = require('./lib/Identity')
-const Directory = require('./lib/Directory')
-const Nonce = require('./lib/Nonce')
-
-// Abstract base request class for carrying out ACME requests.
-class AcmeRequest {
-  async execute (command, payload, useKid=true) {
-    const directory = await Directory.getSharedInstance()
-    const identity = Identity.getSharedInstance()
-    const url = directory[`${command}Url`]
-
-    const protectedHeader = {
-      alg: 'RS256',
-      nonce: await Nonce.get(),
-      url
-    }
-
-    if (useKid) {
-      // The kid is the account location URL as previously returned by the ACME server.
-      protectedHeader.kid = identity.kid
-    } else {
-      // If we’re not using the kid, we must use the public JWK (see RFC 8555 § 6.2 Request Authentication)
-      protectedHeader.jwk = identity.publicJWK
-    }
-
-    const signedRequest = jose.JWS.sign.flattened(payload, identity.key, protectedHeader)
-
-    const httpsHeaders = {
-      'Content-Type': 'application/jose+json',
-      'User-Agent': 'small-tech.org-acme/1.0.0 node/12.16.0',
-      'Accept-Language': 'en-US'
-    }
-
-    // Prepare a new account request (RFC 8555 § 7.3 Account Management)
-    const httpsRequest = prepareRequest('POST', url, /* acceptable responses are */ 200, 201)
-
-    let responseBody
-    let response
-    try {
-      response = await httpsRequest('', signedRequest, httpsHeaders)
-    } catch (error) {
-      console.log('Request error', error)
-      responseBody = error.responseBody
-    }
-    if (responseBody) {
-      const responseBuffer = await responseBody
-      const errorBody = (responseBuffer.toString('utf-8'))
-      console.log('error body', errorBody)
-      throw new Error(errorBody)
-    }
-
-    console.log('ACME Response', response)
-    console.log('Headers', response.headers)
-
-    // Always save the fresh nonce returned from API calls.
-    const freshNonce = response.headers['replay-nonce']
-    Nonce.set(freshNonce)
-
-    return response
-  }
-}
-
-class NewAccountRequest extends AcmeRequest {
-  async execute () {
-    const payload = { termsOfServiceAgreed: true }
-    const response = await super.execute('newAccount', payload, /* useKid = */ false)
-
-    // This is what we will be using in the kid field in the future
-    // **in place of** the JWK.
-    const accountLocation = response.headers['location']
-
-    console.log('Account location', accountLocation)
-  }
-}
-
-// class NewOrderRequest extends AcmeRequest {
-//   execute (payload) { super.execute('newOrder', payload) }
-
-//   process (response) {
-//     // TODO
-//   }
-// }
-
-async function main () {
-
-  await (new NewAccountRequest()).execute()
-
-  // // Generate/save or load the identity.
-  // const identity = Identity.getSharedInstance()
-
-  // // Get the directory.
-  // const directory = await Directory.getSharedInstance()
-
-
-  // newAccount
-  
-
-  // const payload = { termsOfServiceAgreed: true }
-  // const protectedHeader = {
-  //   alg: 'RS256',
-  //   jwk: identity.publicJWK,
-  //   nonce: await Nonce.get(),
-  //   url: directory.newAccountUrl
-  // }
-  // const signedRequest = jose.JWS.sign.flattened(payload, identity.key, protectedHeader)
-
-  // log('Protected header', protectedHeader)
-
-  // // The required headers as per RFC 8555 § 6.1 (HTTPS Requests) and § 6.2 (Request Authentication)
-  // const headers = {
-  //   'Content-Type': 'application/jose+json',
-  //   'User-Agent': 'small-tech.org-acme/1.0.0 node/12.16.0',
-  //   'Accept-Language': 'en-US'
-  // }
-
-  // // Prepare a new account request (RFC 8555 § 7.3 Account Management)
-  // const newAccountRequest = prepareRequest('POST', directory.newAccountUrl, /* acceptable responses are */ 200, 201)
-
-  // log('newAccountRequest', newAccountRequest)
-
-  // let responseBody
-  // let newAccountResponse
-  // try {
-  //   newAccountResponse = await newAccountRequest('', signedRequest, headers)
-  // } catch (error) {
-  //   log('error', error)
-  //   responseBody = error.responseBody
-  // }
-  // if (responseBody) {
-  //   const responseBuffer = await responseBody
-  //   log(responseBuffer.toString('utf-8'))
-  //   process.exit(1)
-  // }
-
-  // log('New account response', newAccountResponse)
-  // log('Headers', newAccountResponse.headers)
-
-  // // Save the latest nonce.
-  // const headers = newAccountResponse.headers
-  // const newNonce = headers['replay-nonce']
-  // Nonce.set(newNonce)
-
-  // // This is what we will be using in the kid field in the future
-  // // **in place of** the JWK.
-  // const accountLocation = headers['location']
-
+class AcmeHttp01 {
   //
-  // newOrder
+  // Singleton access (async).
   //
+  static instance = null
+  static isBeingInstantiatedViaSingletonFactoryMethod = false
 
+  static async getSharedInstance (settingsPath = null) {
+    if (AcmeHttp01.instance === null) {
+      AcmeHttp01.isBeingInstantiatedViaSingletonFactoryMethod = true
+      AcmeHttp01.instance = new AcmeHttp01(settingsPath)
+      await AcmeHttp01.instance.init()
+    }
+    return AcmeHttp01.instance
+  }
 
+  constructor (settingsPath = null) {
+    // Ensure singleton access.
+    if (AcmeHttp01.isBeingInstantiatedViaSingletonFactoryMethod === false) {
+      throw new Error('AcmeHttp01 is a singleton. Please instantiate using the AcmeHttp01.getSharedInstance([settingsPath<str>]) method.')
+    }
+    AcmeHttp01.isBeingInstantiatedViaSingletonFactoryMethod = false
 
+    if (settingsPath === null) {
+      settingsPath = path.join(os.homedir(), '.small-tech.org', 'acme-http-01')
+    }
+    fs.mkdirpSync(settingsPath)
+    this._settingsPath = settingsPath
+  }
+
+  async init () {
+    this.account = await Account.getSharedInstance(this.settingsPath)
+
+    console.log(`Account received. kid = ${this.account.kid}`)
+
+    // TODO
+
+    console.log('Graceful exit. (This module is still under initial development.)')
+  }
+
+  get settingsPath ()      { return this._settingsPath                                   }
+  set settingsPath (value) { throw new Error('The .settingsPath property is read-only.') }
 }
 
-main()
+module.exports = AcmeHttp01
+
+// Dependencies that need to have a reference to this class (e.g., to get the settingsPath),
+// should be required here at the end, _after_ the module.exports line so that they do not
+// crash due to accessing an empty placeholder object.
+// (This is a limitation of Node requires. See https://stackoverflow.com/a/21334734)
+
+const Account = require('./lib/Account')
