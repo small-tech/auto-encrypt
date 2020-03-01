94c54c181995fb753182b5123df5c429c49354d2
Aral Balkan <mail@ar.al>
2020-03-01 21:34:29 +0000

    Add tests for AcmeRequest class

---
+++ test/acme-request.js
@@ -0,0 +1,21 @@
+const test = require('tape')
+const AcmeRequest = require('../lib/AcmeRequest')
+const Account = require('../lib/Account')
+
+test('AcmeRequest', async t => {
+  t.plan = 1
+
+  const { protectedHeader, signedRequest, httpsRequest, httpsHeaders } = await (new AcmeRequest()).prepare('newOrder', {aPayload: true}, /* useKid = */ true)
+
+  const expectedHttpsHeaders = {
+    'Content-Type': 'application/jose+json',
+    'User-Agent': 'small-tech.org-acme/1.0.0 node/12.16.0',
+    'Accept-Language': 'en-US'
+  }
+
+  t.strictEquals(JSON.stringify(httpsHeaders), JSON.stringify(expectedHttpsHeaders), 'https headers should be as expected')
+
+  t.strictEquals(protectedHeader.kid, (await Account.getSharedInstance()).kid, 'account kid should be in protected header by default')
+
+  t.end()
+})
