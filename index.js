f6487addfb95fb39177568df66b276ff9035f7a8
Aral Balkan <mail@ar.al>
2020-03-01 16:54:44 +0000

    Refactor new account request → abstract class/concrete implementation

--- index.js
+++ index.js
@@ -26,55 +26,154 @@ const Identity = require('./lib/Identity')
 const Directory = require('./lib/Directory')
 const Nonce = require('./lib/Nonce')
 
+// Abstract base request class for carrying out ACME requests.
+class AcmeRequest {
+  async execute (command, payload, useKid=true) {
+    const directory = await Directory.getSharedInstance()
+    const identity = Identity.getSharedInstance()
+    const url = directory[`${command}Url`]
+
+    const protectedHeader = {
+      alg: 'RS256',
+      nonce: await Nonce.get(),
+      url
+    }
+
+    if (useKid) {
+      // The kid is the account location URL as previously returned by the ACME server.
+      protectedHeader.kid = identity.kid
+    } else {
+      // If we’re not using the kid, we must use the public JWK (see RFC 8555 § 6.2 Request Authentication)
+      protectedHeader.jwk = identity.publicJWK
+    }
+
+    const signedRequest = jose.JWS.sign.flattened(payload, identity.key, protectedHeader)
+
+    const httpsHeaders = {
+      'Content-Type': 'application/jose+json',
+      'User-Agent': 'small-tech.org-acme/1.0.0 node/12.16.0',
+      'Accept-Language': 'en-US'
+    }
+
+    // Prepare a new account request (RFC 8555 § 7.3 Account Management)
+    const httpsRequest = prepareRequest('POST', url, /* acceptable responses are */ 200, 201)
+
+    let responseBody
+    let response
+    try {
+      response = await httpsRequest('', signedRequest, httpsHeaders)
+    } catch (error) {
+      console.log('Request error', error)
+      responseBody = error.responseBody
+    }
+    if (responseBody) {
+      const responseBuffer = await responseBody
+      const errorBody = (responseBuffer.toString('utf-8'))
+      console.log('error body', errorBody)
+      throw new Error(errorBody)
+    }
+
+    console.log('ACME Response', response)
+    console.log('Headers', response.headers)
+
+    // Always save the fresh nonce returned from API calls.
+    const freshNonce = response.headers['replay-nonce']
+    Nonce.set(freshNonce)
+
+    return response
+  }
+}
+
+class NewAccountRequest extends AcmeRequest {
+  async execute () {
+    const payload = { termsOfServiceAgreed: true }
+    const response = await super.execute('newAccount', payload, /* useKid = */ false)
+
+    // This is what we will be using in the kid field in the future
+    // **in place of** the JWK.
+    const accountLocation = response.headers['location']
+
+    console.log('Account location', accountLocation)
+  }
+}
+
+// class NewOrderRequest extends AcmeRequest {
+//   execute (payload) { super.execute('newOrder', payload) }
+
+//   process (response) {
+//     // TODO
+//   }
+// }
+
 async function main () {
 
-  // Generate/save or load the identity.
-  const identity = Identity.getSharedInstance()
+  await (new NewAccountRequest()).execute()
 
-  // Get the directory.
-  const directory = await Directory.getSharedInstance()
+  // // Generate/save or load the identity.
+  // const identity = Identity.getSharedInstance()
 
-  const payload = { termsOfServiceAgreed: true }
-  const protectedHeader = {
-    alg: 'RS256',
-    jwk: identity.publicJWK,
-    nonce: await Nonce.get(),
-    url: directory.newAccountUrl
-  }
-  const signedRequest = jose.JWS.sign.flattened(payload, identity.key, protectedHeader)
+  // // Get the directory.
+  // const directory = await Directory.getSharedInstance()
 
-  log('Protected header', protectedHeader)
 
-  // The required headers as per RFC 8555 § 6.1 (HTTPS Requests) and § 6.2 (Request Authentication)
-  const headers = {
-    'Content-Type': 'application/jose+json',
-    'User-Agent': 'small-tech.org-acme/1.0.0 node/12.16.0',
-    'Accept-Language': 'en-US'
-  }
+  // newAccount
+  
 
-  // Prepare a new account request (RFC 8555 § 7.3 Account Management)
-  const newAccountRequest = prepareRequest('POST', directory.newAccountUrl, /* acceptable responses are */ 200, 201)
+  // const payload = { termsOfServiceAgreed: true }
+  // const protectedHeader = {
+  //   alg: 'RS256',
+  //   jwk: identity.publicJWK,
+  //   nonce: await Nonce.get(),
+  //   url: directory.newAccountUrl
+  // }
+  // const signedRequest = jose.JWS.sign.flattened(payload, identity.key, protectedHeader)
 
-  log('newAccountRequest', newAccountRequest)
+  // log('Protected header', protectedHeader)
 
-  let responseBody
-  let newAccountResponse
-  try {
-    newAccountResponse = await newAccountRequest('', signedRequest, headers)
-  } catch (error) {
-    log('error', error)
-    responseBody = error.responseBody
-  }
-  if (responseBody) {
-    const responseBuffer = await responseBody
-    log(responseBuffer.toString('utf-8'))
-  }
+  // // The required headers as per RFC 8555 § 6.1 (HTTPS Requests) and § 6.2 (Request Authentication)
+  // const headers = {
+  //   'Content-Type': 'application/jose+json',
+  //   'User-Agent': 'small-tech.org-acme/1.0.0 node/12.16.0',
+  //   'Accept-Language': 'en-US'
+  // }
 
-  log('New account response', newAccountResponse)
-  log('Headers', newAccountResponse.headers)
+  // // Prepare a new account request (RFC 8555 § 7.3 Account Management)
+  // const newAccountRequest = prepareRequest('POST', directory.newAccountUrl, /* acceptable responses are */ 200, 201)
 
-  // TODO: Next we need to save the response.
-  // TODO: Move this into its own class.
+  // log('newAccountRequest', newAccountRequest)
+
+  // let responseBody
+  // let newAccountResponse
+  // try {
+  //   newAccountResponse = await newAccountRequest('', signedRequest, headers)
+  // } catch (error) {
+  //   log('error', error)
+  //   responseBody = error.responseBody
+  // }
+  // if (responseBody) {
+  //   const responseBuffer = await responseBody
+  //   log(responseBuffer.toString('utf-8'))
+  //   process.exit(1)
+  // }
+
+  // log('New account response', newAccountResponse)
+  // log('Headers', newAccountResponse.headers)
+
+  // // Save the latest nonce.
+  // const headers = newAccountResponse.headers
+  // const newNonce = headers['replay-nonce']
+  // Nonce.set(newNonce)
+
+  // // This is what we will be using in the kid field in the future
+  // // **in place of** the JWK.
+  // const accountLocation = headers['location']
+
+  //
+  // newOrder
+  //
+
+
+
 }
 
 main()
