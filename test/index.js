6e47abd421d95d8851b31eb2e1ee18f2635d4c98
Aral Balkan <mail@ar.al>
2020-03-25 14:56:08 +0000

    Add tests for AcmeRequest class

--- test/index.js
+++ test/index.js
@@ -1,31 +1,17 @@
-const os = require('os')
-const fs = require('fs-extra')
-const path = require('path')
-const util = require('util')
-
-const test = require('tape')
-
-const autoEncrypt = require('../index')
-const Configuration = require('../lib/Configuration')
-
-const Directory = require('../lib/Directory')
-const AccountIdentity = require('../lib/AccountIdentity')
-const Account = require('../lib/Account')
-const Nonce = require('../lib/Nonce')
-
-let testSettingsPath
+const os                                            = require('os')
+const fs                                            = require('fs-extra')
+const path                                          = require('path')
+const util                                          = require('util')
+const test                                          = require('tape')
+const autoEncrypt                                   = require('../index')
+const Configuration                                 = require('../lib/Configuration')
+const { destroySingletons, createTestSettingsPath } = require('../lib/test-helpers')
 
 function setup() {
   // Destroy all singleton instances, test paths, and app state so we start with a clean slate.
-  Directory.destroySharedInstance()
-  AccountIdentity.destroySharedInstance()
-  Account.destroySharedInstance()
-  Nonce.destroy()
-
-  testSettingsPath = path.join(os.homedir(), '.small-tech.org', 'auto-encrypt', 'test')
-  fs.removeSync(testSettingsPath)
+  destroySingletons()
+  createTestSettingsPath()
 }
-
 
 test('autoEncrypt', async t => {
   t.plan(11)
---
+++ test/lib/AcmeRequest.js
@@ -0,0 +1,51 @@
+const test                                                         = require('tape')
+const http                                                         = require('http')
+const AcmeRequest                                                  = require('../../lib/AcmeRequest')
+const Account                                                      = require('../../lib/Account')
+const { setupStagingConfigurationWithOneDomainAtTestSettingsPath } = require('../../lib/test-helpers')
+
+test('AcmeRequest', async t => {
+  t.plan(3)
+
+  // Configuration is set up to manage certificates for to dev.ar.al, use the staging server
+  // and store its configuration at the test settings path.
+  setupStagingConfigurationWithOneDomainAtTestSettingsPath()
+
+  const { protectedHeader, signedRequest, httpsRequest, httpsHeaders } = await (new AcmeRequest()).prepare('newOrder', {aPayload: true}, /* useKid = */ true)
+
+  const expectedHttpsHeaders = {
+    'Content-Type': 'application/jose+json',
+    'User-Agent': 'small-tech.org-acme/1.0.0 node/12.16.0',
+    'Accept-Language': 'en-US'
+  }
+
+  t.strictEquals(JSON.stringify(httpsHeaders), JSON.stringify(expectedHttpsHeaders), 'https headers should be as expected')
+
+  t.strictEquals(protectedHeader.kid, (await Account.getSharedInstance()).kid, 'account kid should be in protected header by default')
+
+  // Test call failure.
+  const preparedRequest = await (new AcmeRequest()).prepare(
+    /* command =      */ 'custom-command',
+    /* payload =      */ {aPayload: true},
+    /* useKid =       */ true,
+    /* successCodes = */ [200],
+    /* url =          */ 'http://localhost:3132'
+  )
+
+  const server = http.createServer((request, response) => {
+    response.statusCode = 500
+    response.end()
+  })
+  server.listen(3132)
+
+  try {
+    await (new AcmeRequest())._execute(preparedRequest)
+    t.fail()
+  } catch (error) {
+    t.pass('unexpected server response code should throw')
+  }
+
+  server.close()
+
+  t.end()
+})
