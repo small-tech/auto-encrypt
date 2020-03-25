6a3c9380be162f6e323d9a11280520b87cfdda82
Aral Balkan <mail@ar.al>
2020-03-25 17:45:49 +0000

    Add test for AcmeRequest

--- test/lib/AcmeRequest.js
+++ test/lib/AcmeRequest.js
@@ -1,11 +1,13 @@
 const test                                                         = require('tape')
 const http                                                         = require('http')
+const jose                                                         = require('jose')
 const AcmeRequest                                                  = require('../../lib/AcmeRequest')
 const Account                                                      = require('../../lib/Account')
+const AccountIdentity                                              = require('../../lib/AccountIdentity')
 const { setupStagingConfigurationWithOneDomainAtTestSettingsPath } = require('../../lib/test-helpers')
 
 test('AcmeRequest', async t => {
-  t.plan(3)
+  // t.plan(4)
 
   // Configuration is set up to manage certificates for to dev.ar.al, use the staging server
   // and store its configuration at the test settings path.
@@ -23,7 +25,16 @@ test('AcmeRequest', async t => {
 
   t.strictEquals(protectedHeader.kid, (await Account.getSharedInstance()).kid, 'account kid should be in protected header by default')
 
+  //
+  // Test JSON Web Signature.
+  //
+  const accountIdentity = AccountIdentity.getSharedInstance()
+  const { aPayload } = jose.JWS.verify(signedRequest, accountIdentity.key)
+  t.strictEquals(aPayload, true, 'Payload verifies from the JSON Web Signature')
+
+  //
   // Test call failure.
+  //
   const preparedRequest = await (new AcmeRequest()).prepare(
     /* command =      */ 'custom-command',
     /* payload =      */ {aPayload: true},
@@ -46,6 +57,24 @@ test('AcmeRequest', async t => {
   }
 
   server.close()
+
+  //
+  // Test: parseBodyAsJSON = false
+  //
+  const server2 = http.createServer((request, response) => {
+    response.statusCode = 200
+    response.end('not json')
+  })
+  server2.listen(3132)
+
+  let response
+  try {
+    response = await (new AcmeRequest()).execute('custom-command', {aPayload: true}, true, [200], 'http://localhost:3132', false)
+  } catch (error) {
+    console.log(error)
+  }
+  t.strictEquals(response.body, 'not json', 'body is not JSON as expected')
+  server2.close()
 
   t.end()
 })
