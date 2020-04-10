15c8a7d83197c8860e37082345bb916247bd1407
Aral Balkan <mail@ar.al>
2020-04-10 17:15:13 +0100

    Improve error handling (consistency) and coverage for Identity class

--- test/lib/identities/AccountIdentity.js
+++ test/lib/identities/AccountIdentity.js
@@ -1,12 +1,13 @@
-const os                        = require('os')
-const path                      = require('path')
-const fs                        = require('fs-extra')
-const jose                      = require('jose')
-const test                      = require('tape')
-const AccountIdentity           = require('../../../lib/identities/AccountIdentity')
-const Configuration             = require('../../../lib/Configuration')
-const LetsEncryptServer         = require('../../../lib/LetsEncryptServer')
-const { symbolOfErrorThrownBy } = require('../../../lib/test-helpers')
+const os                                   = require('os')
+const path                                 = require('path')
+const util                                 = require('util')
+const fs                                   = require('fs-extra')
+const jose                                 = require('jose')
+const test                                 = require('tape')
+const AccountIdentity                      = require('../../../lib/identities/AccountIdentity')
+const Configuration                        = require('../../../lib/Configuration')
+const LetsEncryptServer                    = require('../../../lib/LetsEncryptServer')
+const { symbolOfErrorThrownBy, dehydrate } = require('../../../lib/test-helpers')
 
 function setup() {
   // Run the tests using either a local Pebble server (default) or the Let’s Encrypt Staging server
@@ -52,6 +53,30 @@ test('Account Identity', t => {
   t.strictEquals(accountId.thumbprint, accountId.key.thumbprint, 'thumbprint is as expected')
   t.deepEquals(accountId.privateJWK, accountId.key.toJWK(/* private = */ true), 'private JWK is as expected')
   t.deepEquals(accountId.publicJWK , accountId.key.toJWK(/* private = */ false), 'public JWK is as expected')
+
+  // Test that read-only setters are actually read-only.
+  ;['key', 'privatePEM', 'thumbprint', 'privateJWK', 'publicJWK', 'filePath'].forEach(readOnlySetter => {
+    t.strictEquals(
+      symbolOfErrorThrownBy(() => accountId[readOnlySetter] = 'dummy value'),
+      Symbol.for('ReadOnlyAccessorError'),
+      `attempt to set read-only property ${readOnlySetter} throws`
+    )
+  })
+
+  const expectedInspectionString = dehydrate(`# Identity
+  Generates, stores, loads, and saves an identity (JWT OKP key using
+  Ed25519 curve) from/to file storage.
+  - Identity file path: /home/aral/.small-tech.org/auto-encrypt/test/pebble/account-identity.pem
+  ## Properties
+  - .key        : the jose.JWK.RSAKey instance
+  - .privatePEM : PEM representation of the private key
+  - .thumbprint : JWK thumbprint calculated according to RFC 7638
+  - .privateJWK : JavaScript object representation of JWK (private key)
+  - .publicJWK  : JavaScript object representation of JWK (public key)
+  - .filePath   : The file path of the private key (saved in PEM format)
+  To see key details, please log() the .key property.`)
+
+  t.strictEquals(dehydrate(util.inspect(accountId)), expectedInspectionString, 'the inspection string is as expected')
 
   t.end()
 })
