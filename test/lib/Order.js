8ba0e0e58f5494136af935df8bc2740ea729e5a2
Aral Balkan <mail@ar.al>
2020-04-12 16:53:10 +0100

    Add initial Order class tests; Order class now throws SymbolicErrors

---
+++ test/lib/Order.js
@@ -0,0 +1,58 @@
+const os                        = require('os')
+const fs                        = require('fs-extra')
+const path                      = require('path')
+const test                      = require('tape')
+const Order                     = require('../../lib/Order')
+const Configuration             = require('../../lib/Configuration')
+const Directory                 = require('../../lib/Directory')
+const Account                   = require('../../lib/Account')
+const AccountIdentity           = require('../../lib/identities/AccountIdentity')
+const AcmeRequest               = require('../../lib/AcmeRequest')
+const LetsEncryptServer         = require('../../lib/LetsEncryptServer')
+const { symbolOfErrorThrownBy } = require('../../lib/test-helpers')
+
+async function setup() {
+  // Run the tests using either a local Pebble server (default) or the Let’s Encrypt Staging server
+  // (which is subject to rate limits) if the STAGING environment variable is set.
+  // Use npm test task for the former and npm run test-staging task for the latter.
+  const letsEncryptServerType = process.env.STAGING ? LetsEncryptServer.type.STAGING : LetsEncryptServer.type.PEBBLE
+
+  const customSettingsPath = path.join(os.homedir(), '.small-tech.org', 'auto-encrypt', 'test')
+  fs.removeSync(customSettingsPath)
+
+  const configuration = new Configuration({
+    domains: ['dev.ar.al'],
+    server: new LetsEncryptServer(letsEncryptServerType),
+    settingsPath: customSettingsPath
+  })
+
+  const directory = await Directory.getInstanceAsync(configuration)
+  const accountIdentity = new AccountIdentity(configuration)
+  AcmeRequest.initialise(directory, accountIdentity)
+  const account = await Account.getInstanceAsync(configuration)
+  AcmeRequest.account = account
+
+  return { configuration, accountIdentity }
+}
+
+test('Order', async t => {
+  const { configuration, accountIdentity } = await setup()
+
+  const order = await Order.getInstanceAsync(configuration, accountIdentity)
+
+  //
+  // Test read-only setters.
+  //
+
+  const readOnlySetters = ['certificate', 'certificateIdentity', 'authorisations', 'finaliseUrl', 'identifiers', 'authorisations', 'status', 'expires', 'certificateUrl', 'headers']
+
+  readOnlySetters.forEach(readOnlySetter => {
+    t.strictEquals(
+      symbolOfErrorThrownBy(() => { order[readOnlySetter] = 'dummy value' }),
+      Symbol.for('ReadOnlyAccessorError'),
+      `trying to set read-only property ${readOnlySetter} throws`
+    )
+  })
+
+  t.end()
+})
