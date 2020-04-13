53130f13331f2f1ada6324945840e8e3ac48a46d
Aral Balkan <mail@ar.al>
2020-04-13 17:55:23 +0100

    Improve tests

--- test/lib/acme-requests/AuthorisationRequest.js
+++ test/lib/acme-requests/AuthorisationRequest.js
@@ -1,47 +1,6 @@
-const os                             = require('os')
-const fs                             = require('fs-extra')
-const path                           = require('path')
-const util                           = require('util')
-const nock                           = require('nock')
 const test                           = require('tape')
 const AuthorisationRequest           = require('../../../lib/acme-requests/AuthorisationRequest')
-const Directory                      = require('../../../lib/Directory')
-const Account                        = require('../../../lib/Account')
-const AccountIdentity                = require('../../../lib/identities/AccountIdentity')
-const AcmeRequest                    = require('../../../lib/AcmeRequest')
-const Configuration                  = require('../../../lib/Configuration')
-const LetsEncryptServer              = require('../../../lib/LetsEncryptServer')
-const { symbolOfErrorThrownBy,
-        symbolOfErrorThrownByAsync } = require('../../../lib/test-helpers')
-
-async function setup() {
-  // Run the tests using either a local Pebble server (default) or the Let’s Encrypt Staging server
-  // (which is subject to rate limits) if the STAGING environment variable is set.
-  // Use npm test task for the former and npm run test-staging task for the latter.
-  const letsEncryptServerType = process.env.STAGING ? LetsEncryptServer.type.STAGING : LetsEncryptServer.type.PEBBLE
-
-  const domains = {
-    [LetsEncryptServer.type.PEBBLE]: ['localhost', 'pebble'],
-    [LetsEncryptServer.type.STAGING]: [os.hostname(), `www.${os.hostname()}`]
-  }
-
-  const customSettingsPath = path.join(os.homedir(), '.small-tech.org', 'auto-encrypt', 'test')
-  fs.removeSync(customSettingsPath)
-
-  const configuration = new Configuration({
-    domains: domains[letsEncryptServerType],
-    server: new LetsEncryptServer(letsEncryptServerType),
-    settingsPath: customSettingsPath
-  })
-  const directory = await Directory.getInstanceAsync(configuration)
-
-  const accountIdentity = new AccountIdentity(configuration)
-  AcmeRequest.initialise(directory, accountIdentity)
-  const account = await Account.getInstanceAsync(configuration)
-  AcmeRequest.account = account
-
-  return configuration
-}
+const { symbolOfErrorThrownByAsync } = require('../../../lib/test-helpers')
 
 test('Authorisation Request', async t => {
   t.strictEquals(
---
+++ test/lib/acme-requests/NewAccountRequest.js
@@ -0,0 +1,8 @@
+const test                           = require('tape')
+const NewAccountRequest              = require('../../../lib/acme-requests/NewAccountRequest')
+const { symbolOfErrorThrownByAsync } = require('../../../lib/test-helpers')
+
+test('New Account Request', async t => {
+  t.pass('(no unit-specific tests)')
+  t.end()
+})
