98c3b034e7fe492ba784a3936bb43f4cd04a7dae
Aral Balkan <mail@ar.al>
2020-04-13 17:59:36 +0100

    Add ReadyForChallengeValidationRequest test; improve error handling

---
+++ test/lib/acme-requests/ReadyForChallengeValidationRequest.js
@@ -0,0 +1,13 @@
+const test                               = require('tape')
+const ReadyForChallengeValidationRequest = require('../../../lib/acme-requests/ReadyForChallengeValidationRequest')
+const { symbolOfErrorThrownByAsync }     = require('../../../lib/test-helpers')
+
+test('Authorisation Request', async t => {
+  t.strictEquals(
+    await symbolOfErrorThrownByAsync(async () => { await (new ReadyForChallengeValidationRequest()).execute() }),
+    Symbol.for('UndefinedOrNullError'),
+    'attempting to execute ready for challenge validation request without challenge url argument throws as expected'
+  )
+
+  t.end()
+})
